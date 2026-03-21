53d5a47226885fa587ac8ea13f896a27a47e7afb
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-03-21 14:46:07 +0100

    Added DependencyInjection and ServiceRegistration.

---
+++ Application/DependencyInjection/ApplicationDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Application.DependencyInjection;
+
+public static class ApplicationDependencyInjection
+{
+    public static IServiceCollection AddApplication (this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configuration);
+        ArgumentNullException.ThrowIfNull(environment);
+
+        services.AddServices();
+
+        return services;
+    }
+}
---
+++ Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.DependencyInjection;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        return services;
+    }
+}
