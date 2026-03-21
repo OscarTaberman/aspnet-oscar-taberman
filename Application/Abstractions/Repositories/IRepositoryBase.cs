0418c191f8712aae71b5a3b3904bc14309e391f1
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-03-21 16:54:57 +0100

    Added IRepositoryBase.

---
+++ Application/Abstractions/Repositories/IRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Abstractions.Repositories;
+
+public interface IRepositoryBase<TModel, TId>
+{
+    Task<TModel> AddAsync(TModel model, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<TModel>> GetAllAsync(CancellationToken ct = default);
+    Task<TModel?> GetByIdAsync(TId id, CancellationToken cancellationToken = default);
+    Task<TModel> UpdateAsync(TId id, TModel model, CancellationToken cancellationToken = default);
+    Task<bool> RemoveAsync(TId id, CancellationToken cancellationToken = default);
+}
