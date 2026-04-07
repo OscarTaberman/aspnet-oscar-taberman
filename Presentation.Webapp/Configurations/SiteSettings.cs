f4de8561e6c97afa9c5a731fa8a761e363edf2ee
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-07 15:44:17 +0200

    Added SiteSettings. Added Partials _Header, _Footer.

---
+++ Presentation.Webapp/Configurations/SiteSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.Webapp.Configurations;
+
+public sealed class SiteSettings
+{
+    public required string SiteName { get; init; }
+    public required string Slogan { get; init; }
+    public required string Description { get; init; }
+    public required string Language { get; init; }
+}
--- Presentation.Webapp/Program.cs
+++ Presentation.Webapp/Program.cs
@@ -1,10 +1,12 @@
 using Application.DependencyInjection;
 using Infrastructure.DependencyInjection;
 using Infrastructure.Persistence;
+using Presentation.Webapp.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<SiteSettings>(builder.Configuration.GetSection("SiteSettings"));
 
 builder.Services.AddApplication(builder.Configuration, builder.Environment);
 builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
