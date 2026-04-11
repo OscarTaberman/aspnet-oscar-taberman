c03c93c4e7508b5518e95dd895a6853b82ab1695
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-11 11:31:21 +0200

    Added AccountController with views for Register, SignIn and SetPassword.

---
+++ Presentation.Webapp/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Webapp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("account")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
