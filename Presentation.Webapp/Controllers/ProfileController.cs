1533c90fd48cb88c9c4087220f0a25f9e22e47e9
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-19 11:40:23 +0200

    Added ProfileImage to ApplicationUser with migration/update. Added css-styling profile-form. Added ExistingProfileImage-property to ProfileViewModel.

--- Presentation.Webapp/Controllers/ProfileController.cs
+++ Presentation.Webapp/Controllers/ProfileController.cs
@@ -1,22 +1,55 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Webapp.ViewModels;
 
 namespace Presentation.Webapp.Controllers
 {
     [Authorize]
-    public class ProfileController : Controller
+    public class ProfileController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : Controller
     {
         [HttpGet]
         [Route("profile")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var profileViewModel = new ProfileViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email ?? string.Empty,
+                PhoneNumber = user.PhoneNumber,
+                ExistingProfileImageUrl = null,
+            };
+
+            return View(profileViewModel);
         }
 
         [HttpPost]
         [Route("profile")]
-        public IActionResult UpdateProfile()
+        public async Task<IActionResult> UpdateProfile()
         {
+            var user = await userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            var updateUser = new ProfileViewModel
+            {
+                FirstName = user.FirstName ?? string.Empty,
+                LastName = user.LastName ?? string.Empty,
+                Email = user.Email ?? string.Empty,
+                PhoneNumber = user.PhoneNumber,
+                ExistingProfileImageUrl = null,
+            };
+
             return RedirectToAction("Index");
         }
     }
--- Presentation.Webapp/ViewModels/ProfileViewModel.cs
+++ Presentation.Webapp/ViewModels/ProfileViewModel.cs
@@ -11,20 +11,18 @@ public sealed class ProfileViewModel
     [Display(Name = "Last Name", Prompt = "Enter Last Name")]
     [Required(ErrorMessage = "Last Name is required.")]
     public string? LastName { get; set; } = null!;
+
     [EmailAddress(ErrorMessage = "Invalid email address.")]
     [Display(Name = "Email Address", Prompt = "Enter Email Address")]
     [Required(ErrorMessage = "Email is required.")]
     public string Email { get; set; } = null!;
-
-    [DataType(DataType.Password, ErrorMessage = "Invalid password.")]
-    [Display(Name = "Password", Prompt = "Enter Password")]
-    [Required(ErrorMessage = "Password is required.")]
-    public string? Password { get; set; } = null!;
 
     [Phone(ErrorMessage = "Invalid phone number.")]
     [Display(Name = "Phone Number", Prompt = "Enter Phone Number")]
     public string? PhoneNumber { get; set; } = null!;
 
     [Display(Name = "Profile Picture")]
-    public string? ProfilePicture { get; set; } = null!;
+    public IFormFile? ProfileImage { get; set; }
+
+    public string? ExistingProfileImageUrl { get; set; }
 }
