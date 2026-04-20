f2f10cd54d21f46f08f2d3b6ee62e173bda4747d
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-20 05:04:42 +0200

    Added controller, view for Membership, CustomerService.

---
+++ Presentation.Webapp/Controllers/CustomerService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Webapp.Controllers;
+
+public class CustomerService : Controller
+{
+    [Route("customer-service")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Presentation.Webapp/Controllers/MembershipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Webapp.Controllers;
+
+public class MembershipController : Controller
+{
+    [Route("membership")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
